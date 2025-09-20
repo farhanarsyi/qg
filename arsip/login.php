f3b50f3c9bde2e27f35c2494f58fca58947e5035
farhanarsyi <farhanarsyi@gmail.com>
2025-09-20 07:54:08 +0700

    hapus file tidak terpakai

--- arsip/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-// login.php - Redirect to SSO (Legacy Login System Deprecated)
-session_start();
-
-// Sistem login lama sudah tidak digunakan lagi  
-// Semua autentikasi sekarang menggunakan SSO BPS
-header('Location: sso_login.php');
-exit;
-?> 
